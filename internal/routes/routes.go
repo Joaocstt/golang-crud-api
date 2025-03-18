653453c059ad8dcdf3e070a96af874e9ee6aa9da
João <80222326+Joaocstt@users.noreply.github.com>
2025-03-17 22:31:15 -0300

    Update routes.go

--- internal/routes/routes.go
+++ internal/routes/routes.go
@@ -7,6 +7,6 @@ import (
 
 // InitRoutes configura as rotas do servidor.
 func InitRoutes(c *gin.Engine) {
-	// Associando a rota "/" (página inicial) à função HomeController do pacote controllers.
+	// Associando a rota "/" (página inicial) à função na Handler, para executar o fluxo da criação de usuário
 	c.POST("/", handlers.PostUser)
 }
